6c4e3a8ff70dbc4e094eb2de0f0d166e392eddc2
Wongsathorn632115035 <wongsathorn_w@cmu.ac.th>
2022-09-16 03:12:58 +0700

    8.11

--- src/router/index.js
+++ src/router/index.js
@@ -10,7 +10,9 @@ import NetWorkErrorView from '@/views/NetworkErrorView.vue'
 import NProgress from 'nprogress'
 import GStore from '@/store'
 import EventService from '@/services/EventService'
-import AddEvent from '@/views/EventForm'
+import AddEvent from '@/views/EventForm.vue'
+import AddOrganizer from '@/views/OrganizerForm.vue'
+import OrganizerListView from '@/views/OrganizerListView.vue'
 
 const routes = [
   {
@@ -26,10 +28,19 @@ const routes = [
   },
   {
     path: '/add-event',
-
     name: 'AddEvent',
-
     component: AddEvent
+  },
+  {
+    path: '/add-organizer',
+    name: 'AddOrganizer',
+    component: AddOrganizer
+  },
+  {
+    path: '/organizers',
+    name: 'OrganizerList',
+    component: OrganizerListView,
+    props: (route) => ({ page: parseInt(route.query.page) || 1 })
   },
   {
     path: '/event/:id',
